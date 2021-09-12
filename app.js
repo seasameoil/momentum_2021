835a76c538b6bbcd6f4b15d68f789bd3cb490507
seasameoil <whdwlgus12@gmail.com>
2021-09-12 11:08:06 +0900

    2021/09/12

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-const title = document.getElementById("title");
+const h1 = document.querySelector(".hello h1");
 
-title.innerText = "Got you!";
+function handleTitleClick() {
+  const currentColor = h1.style.color;
+  let newColor;
+  if (currentColor === "blue") {
+    newColor = "tomato";
+  } else {
+    newColor = "blue";
+  }
+  h1.style.color = newColor;
+}
 
-console.log(title.id);
+h1.addEventListener("click", handleTitleClick);
