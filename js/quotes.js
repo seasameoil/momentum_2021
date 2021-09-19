26c7be58965027943650d6745d3fa8f000b0bd54
seasameoil <whdwlgus12@gmail.com>
2021-09-19 23:57:33 +0900

    2021/09/19

--- js/quotes.js
+++ js/quotes.js
@@ -1,23 +1,22 @@
 const quotes = [
   {
     quote: "I don’t want to survive. Want to live the life I want.",
-    author: "?",
   },
-  { quote: "The less their ability, the more their conceit.", author: "?" },
-  { quote: "Only you can change yourself.", author: "?" },
-  { quote: "Don’t dream it, be it.", author: "?" },
-  { quote: "Only you can change yourself.", author: "?" },
-  { quote: "Time you enjoy wasting is not wasted time.", author: "?" },
-  { quote: "A day without laughter is a day wasted.", author: "?" },
-  { quote: "We can give advice, but we cannot give conduct.", author: "?" },
-  { quote: "Who dares, wins.", author: "?" },
-  { quote: "Change before you have to.", author: "?" },
+  { quote: "The less their ability, the more their conceit." },
+  { quote: "Only you can change yourself." },
+  { quote: "Don’t dream it, be it." },
+  { quote: "Only you can change yourself." },
+  { quote: "Time you enjoy wasting is not wasted time." },
+  { quote: "A day without laughter is a day wasted." },
+  { quote: "We can give advice, but we cannot give conduct." },
+  { quote: "Who dares, wins." },
+  { quote: "Change before you have to." },
 ];
 
 const quote = document.querySelector("#quote span:first-child");
-const author = document.querySelector("#quote span:last-child");
+//const author = document.querySelector("#quote span:last-child");
 
 const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
 quote.innerText = todaysQuote.quote;
-author.innerText = todaysQuote.author;
+//author.innerText = todaysQuote.author;
